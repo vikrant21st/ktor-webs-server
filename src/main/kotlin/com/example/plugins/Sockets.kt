1aedf3db8bae13d978e210fc667164ff87a5bb9a
vikrant21st <vikrant21st>
2023-01-23 22:36:02 +0530

    Robot mouse

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -1,25 +1,25 @@
 package com.example.plugins
 
-import io.ktor.server.websocket.*
-import io.ktor.websocket.*
-import java.time.Duration
+import com.example.robot.RobotGo
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
 import io.ktor.utils.io.*
+import io.ktor.websocket.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.InputStream
 import java.util.*
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
 
 fun Application.configureSockets() {
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
+       /* pingPeriod = Duration.ofSeconds(15)
         timeout = Duration.ofSeconds(15)
         maxFrameSize = Long.MAX_VALUE
-        masking = false
+        masking = false*/
     }
     
     routing {
@@ -27,7 +27,8 @@ fun Application.configureSockets() {
             for (frame in incoming) {
                 if (frame is Frame.Text) {
                     val text = frame.readText()
-                    outgoing.send(Frame.Text("YOU SAID: $text"))
+                    RobotGo.process(text)
+                    outgoing.send(Frame.Text("ACK"))
                     if (text.equals("bye", ignoreCase = true)) {
                         close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
                     }
---
+++ src/main/kotlin/com/example/robot/RobotGo.kt
@@ -0,0 +1,51 @@
+package com.example.robot
+
+import androidx.compose.ui.geometry.Offset
+import com.example.eventWraps.MouseClick
+import com.example.eventWraps.ReleaseEvent
+import io.ktor.util.collections.*
+import kotlinx.coroutines.*
+import java.awt.Robot
+
+object RobotGo {
+    private val robot = Robot().apply { autoDelay = (5) }
+    private val mouseButtonsPressed = ConcurrentSet<Int>()
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    suspend fun process(command: String) =
+        scope.async {
+            if (command == ReleaseEvent.toString())
+                mouseButtonsPressed.forEach { robot.mouseRelease(it) }
+            else if (command.startsWith("Mouse"))
+                MouseClick.fromString(command)?.run {
+                    moveMouseTo(position)
+
+                    if (this is MouseClick) {
+                        if (clickCount > 1)
+                            repeat(clickCount) {
+                                pressMouse(button)
+                                delay(5)
+                                releaseMouse(button)
+                            }
+                        else if (pressed)
+                            pressMouse(button)
+                        else
+                            releaseMouse(button)
+                    }
+                } ?: println("NOT PARSED: $command")
+        }.await()
+
+    private fun releaseMouse(button: Int) {
+        robot.mouseRelease(button)
+        mouseButtonsPressed -= button
+    }
+
+    private fun pressMouse(button: Int) {
+        robot.mousePress(button)
+        mouseButtonsPressed += button
+    }
+
+    private fun moveMouseTo(position: Offset) {
+        position.run { robot.mouseMove(x.toInt(), y.toInt()) }
+    }
+}
